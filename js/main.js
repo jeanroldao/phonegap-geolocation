d49e5f90dfc4881c55c4b31fd93a971b513a9140
Jean Farias Roldão <jeanroldao@gmail.com>
2015-09-07 22:36:34 -0300

    Input time crosswalk test
    Signed-off-by: Jean Farias Roldão <jeanroldao@gmail.com>

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@
 (function(angular) {
   var app = angular.module('geoApp', []);
   
-  app.controller('indexController', function($scope) {
+  app.controller('indexController', function() {
     var index = this;
     
     index.title = 'Geolocation Test';
     index.description = 'This is a geolocation test.';
+    
+    index.time = new Date();
     
     index.browserAgent = navigator.userAgent;
   });
